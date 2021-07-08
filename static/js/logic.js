e6c69fed58077cd8bc4a026265944497a06c8b99
Agust <agusterlingsson@gmail.com>
2021-07-08 15:18:22 -0500

    incomplete code 2

--- static/js/logic.js
+++ static/js/logic.js
@@ -25,6 +25,19 @@ var lightmap = L.map("map", {
     accessToken: API_KEY
   }).addTo(lightmap);
 
+  var lightmap = L.map("map", {
+    center: USCoords,
+    zoom: mapZoomLevel
+  });
+  L.sateliteLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+  });
+
 //create the base layers
 var baseLayers = {
     "Light": lightmap
